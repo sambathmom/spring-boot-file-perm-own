9f0f0714d030339b365e45ef1cc1a5703c980130
sambath.mom <sambath.mom@amkcambodia.com>
2022-07-12 14:08:10 +0700

    first commit

---
+++ src/main/java/com/example/springbootfilepermown/SpringBootFilePermOwnApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootfilepermown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFilePermOwnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFilePermOwnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootfilepermown/SpringBootFilePermOwnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootfilepermown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFilePermOwnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
